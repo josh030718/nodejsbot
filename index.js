29864874d7a758e50c6f5d2075098698549015b1
josh030718 <josh030718@naver.com>
2020-10-03 03:00:26 +0900

    bot upload

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ client.on("guildMemberAdd", (member) => {
 
   welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
 
-  member.addRole(guild.roles.find(role => role.name == "게스트"));
+  member.addRole(guild.roles.find(role => role.name == "게스트")); // 역할부여
 });
 
 client.on("guildMemberRemove", (member) => {
   const guild = member.guild;
   const deleteUser = member.user;
-  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName); //인사
 
   byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
 });
@@ -31,9 +31,33 @@ client.on("guildMemberRemove", (member) => {
 client.on('message', (message) => {
   if(message.author.bot) return;
 
-  if(message.content === 'ping') {
-    message.reply('pong');
+  if(message.content == 'ping') {
+    return message.reply('pong'); //말 주고받기
+  }
+
+  if(message.content.startsWith('!전체공지')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // 채널에서 공지 쓸 때
+      let contents = message.content.slice('!전체공지'.length);
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(`<@${message.author.id}> ${contents}`);
+      });
+  
+      return message.reply('공지를 전송했습니다.');
+    } else {
+      return message.reply('채널에서 실행해주세요.');
+    }
   }
 });
+
+function checkPermission(message) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+    return true;
+  } else {
+    return false;
+  }
+}
 
 client.login(token);
