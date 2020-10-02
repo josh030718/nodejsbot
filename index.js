8fdb9cf692972bc59f9d9d155fa4ee5008bf975f
josh030718 <josh030718@naver.com>
2020-10-03 04:06:33 +0900

    bot upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
-  client.user.setPresence({ game: { name: '어쩌라고 못생긴게' }, status: 'online' })
+  client.user.setPresence({ game: { name: '뒹굴뒹굴' }, status: 'online' })
 });
 
 client.on("guildMemberAdd", (member) => {
